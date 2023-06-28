84adff5ef95bf39cfd6ddd96a81a3ddbcf6da7d2
thelight0804 <thelight0804@gmail.com>
2023-06-28 22:55:02 +0900

    Create project

---
+++ src/test/java/com/gdsc/thelight0804/blogapi/BlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdsc.thelight0804.blogapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
